fb52fc43ecc4990290d6054f5f3f4bc50beadda9
cas254 <Cecilie.Agergaard@mrc-epid.cam.ac.uk>
2024-12-17 11:14:04 +0000

    Merged Appending summary/daily/hourly files into Appending_Files.py which can append all level data.

--- Appending_Files.py
+++ Appending_Files.py
@@ -8,6 +8,7 @@
 import config
 import os
 import pandas as pd
+import Wave_PostProcessingOrchestra
 
 
 ############################################################################################################
@@ -15,65 +16,74 @@ import pandas as pd
 ############################################################################################################
 
 # CREATING A FILE LIST OF ALL FILES IN THE RESULTS FOLDER #
-def create_filelist():
+def create_filelist(folder):
 
     # DELETE ALL PAST FILELISTS IN INDIVIDUAL SUMMARY FOLDER
-    SUMMARY_FOLDER_PATH = os.path.join(config.ROOT_FOLDER, config.RESULTS_FOLDER, config.SUMMARY_FOLDER, config.INDIVIDUAL_SUM_F, config.TIME_RES_FOLDER)
+    file_path = os.path.join(config.ROOT_FOLDER, config.RESULTS_FOLDER, config.SUMMARY_FOLDER, folder, config.TIME_RES_FOLDER)
 
-    if os.path.exists(os.path.join(SUMMARY_FOLDER_PATH, "filelist.txt")):  # If any filelist in the summary folder, this will be deleted.
-        os.remove(os.path.join(SUMMARY_FOLDER_PATH, "filelist.txt"))
+    if os.path.exists(os.path.join(file_path, "filelist.txt")):  # If any filelist in the summary folder, this will be deleted.
+        os.remove(os.path.join(file_path, "filelist.txt"))
 
     # CREATING NEW FILELIST IN INDIVIDUAL SUMMARY FOLDER - CODE IS DIFFERENT FOR MAC AND WINDOWS, CHANGE PC TYPE IN THE BEGINNING OF SCRIPT
-    os.chdir(SUMMARY_FOLDER_PATH)
+    os.chdir(file_path)
 
     if config.PC_TYPE.lower() == "windows":
-        os.system('dir /b *csv > filelist.txt')
+        os.system('dir /b *.csv > filelist.txt')
     elif config.PC_TYPE.lower() == "mac":
         os.system('ls /b *csv > filelist.txt')
 
+    return file_path
+
 # REMOVING FILES THAT SHOULD NOT BE APPENDED #
-def remove_files():
+def remove_files(output_file):
     filelist_df = pd.read_csv('filelist.txt', header=None, names=['v1'])  # Reading in the filelist
     filelist_df = filelist_df.rename(columns={'v1': 'file_name'})
 
     # Dropping the data dictionary from the list and also the list of the main output file if saved in the same location so it is not looped through
     data_dictionary = 'dictionary'
     filelist_df = filelist_df[~filelist_df['file_name'].str.contains(data_dictionary, case=False, na=False)]
-    filelist_df = filelist_df[~filelist_df['file_name'].str.contains(config.SUM_OUTPUT_FILE, case=False, na=False)]
+    filelist_df = filelist_df[~filelist_df['file_name'].str.contains(output_file, case=False, na=False)]
 
     # Creating list with all filenames
     files_list = filelist_df['file_name'].tolist()
     return files_list
 
 # Appending summary files
-def appending_files(files_list):
+def appending_files(files_list, file_path, append_level):
     dataframes = []
 
     # Looping though each file and appending
-    for file_name in files_list:
-        file_path = os.path.join(config.ROOT_FOLDER, config.RESULTS_FOLDER, config.SUMMARY_FOLDER, config.INDIVIDUAL_SUM_F, config.TIME_RES_FOLDER, f"{file_name}")
+    if files_list:
+        for file_name in files_list:
+            full_file_path = os.path.join(file_path, f"{file_name}")
 
-        if os.path.exists(file_path):
-            dataframe = pd.read_csv(file_path)
-            dataframes.append(dataframe)
+            if os.path.exists(full_file_path):
+                dataframe = pd.read_csv(full_file_path)
+                dataframes.append(dataframe)
 
     if dataframes:
         appended_df = pd.concat(dataframes, ignore_index=True)
     else:
         appended_df = pd.DataFrame()
 
+    if append_level == 'hourly':
+        appended_df['id'] = appended_df['file_id']
+
     # DROP ANY FILE WITH NO ID
     appended_df = appended_df.dropna(subset=['id'])
 
     # REMOVING THRESHOLDS FROM THE MAIN OUTPUT FILE IF THIS IS SPECIFIED IN CONFIG FILE
     if config.REMOVE_THRESHOLDS.lower() == 'yes':
-        variable_prefix = 'enmo_'
+        variable_prefixes = 'enmo_'
+        if not any(item.lower() == "hpfvm" for item in config.VARIABLES_TO_DROP):
+            variable_prefixes += 'HPFVM_'
         variable_suffix = 'plus'
 
         columns_to_drop = []
-        for column_name in appended_df.columns:
-            if column_name.startswith(variable_prefix) and column_name.endswith(variable_suffix):
-                columns_to_drop.append(column_name)
+        for variable_prefix in variable_prefixes:
+            for column_name in appended_df.columns:
+                if column_name.startswith(variable_prefix) and column_name.endswith(variable_suffix):
+                    columns_to_drop.append(column_name)
 
         appended_df = appended_df.drop(columns=columns_to_drop)
     return appended_df
@@ -83,29 +93,29 @@ def no_analysis_filelist():
     no_analysis_path = os.path.join(config.ROOT_FOLDER, config.RESULTS_FOLDER, config.FILELIST_FOLDER, 'No_Analysis_Files.txt')
     if os.path.exists(no_analysis_path):
 
-        no_analysis_filelist_df = pd.read_csv('No_Analysis_Files.txt', delimiter='\t')  # Reading in the filelist
+        no_analysis_filelist_df = pd.read_csv(no_analysis_path)  # Reading in the filelist
         no_analysis_filelist_df = no_analysis_filelist_df.drop_duplicates(subset=['filename_temp'])
-        no_analysis_filelist = no_analysis_filelist_df['filename_temp'].tolist()
+        no_analysis_files = no_analysis_filelist_df['filename_temp'].tolist()
 
-        return no_analysis_filelist
+        return no_analysis_files
 
     else:
         return []
 
 
 # Appending any IDS that have not had an analysis file produced from post processing and outputting the dataset
-def appending_no_analysis_files(no_analysis_filelist, appended_df):
+def appending_no_analysis_files(no_analysis_files, appended_df, file_name):
     no_analysis_dataframes = []
-    if not no_analysis_filelist:
-        print("All files had a metadata and data file. No extra data to append")
+    if not no_analysis_files:
+        print("All files had a metadata and data file. No extra data to append.")
         output_file_path = os.path.join(config.ROOT_FOLDER, config.RESULTS_FOLDER, config.SUMMARY_FOLDER)
         os.makedirs(output_file_path, exist_ok=True)
-        file_name = os.path.join(output_file_path, f"{config.SUM_OUTPUT_FILE}.csv")
+        file_name = os.path.join(output_file_path, f"{file_name}.csv")
         appended_df.to_csv(file_name, index=False)
 
         return
 
-    for file_id in no_analysis_filelist:
+    for file_id in no_analysis_files:
 
         metadata_file_path = os.path.join(config.ROOT_FOLDER, config.RESULTS_FOLDER, f"metadata_{file_id}.csv")
         if os.path.exists(metadata_file_path):
@@ -114,9 +124,15 @@ def appending_no_analysis_files(no_analysis_filelist, appended_df):
             # Specifying what variables to keep
             variables_to_keep = [
                 '.*start_error*.', '.*end_error*.', 'calibration_method', 'noise_cutoff_mg',
-                'generic_first_timestamp', 'generic_last_timestamp', 'device', 'processing_epoch',
-                '.*anom*.', '.*batt*.'
+                'generic_first_timestamp', 'generic_last_timestamp', 'device', 'processing_epoch'
             ]
+            # Variables to keep if processed through Wave
+            if config.PROCESSING.lower() == 'wave':
+                variables_to_keep.extend(['.*anom*.', '.*batt*.'])
+            # Variables to keep if processed through Pampro
+            if config.PROCESSING.lower() == 'pampro':
+                variables_to_keep.extend(['calibration_type'])
+
             # Joining the variables to be able to use regular expression
             combined_variables = '|'.join(variables_to_keep)
             no_analysis_metadata_df = no_analysis_metadata_df.filter(regex=combined_variables)
@@ -129,7 +145,12 @@ def appending_no_analysis_files(no_analysis_filelist, appended_df):
             no_analysis_metadata_df = no_analysis_metadata_df.rename(columns=lower_case_mapping)
 
             # Dropping variables:
-            no_analysis_metadata_df.drop(columns=['first_battery', 'last_battery'], inplace=True)
+            if config.PROCESSING.lower() == 'wave':
+                no_analysis_metadata_df.drop(columns=['first_battery', 'last_battery'], inplace=True)
+
+            # Formatting time stamp variables:
+            generic_timestamps = ['generic_first_timestamp', 'generic_last_timestamp']
+            no_analysis_metadata_df[generic_timestamps] = no_analysis_metadata_df[generic_timestamps].apply(lambda x: x.str[:19])
 
             no_analysis_dataframes.append(no_analysis_metadata_df)
 
@@ -140,21 +161,38 @@ def appending_no_analysis_files(no_analysis_filelist, appended_df):
             appended_no_analysis_df = pd.DataFrame()
 
         # appending the dataset from no_analysis with the ones that have analysis data.
-        merged_summary_df = pd.concat([appended_df, appended_no_analysis_df], ignore_index=True)
+        merged_df = pd.concat([appended_df, appended_no_analysis_df], ignore_index=True)
 
         # Outputting appended summary dataframe
         output_file_path = os.path.join(config.ROOT_FOLDER, config.RESULTS_FOLDER, config.SUMMARY_FOLDER)
         os.makedirs(output_file_path, exist_ok=True)
-        file_name = os.path.join(output_file_path, f"{config.SUM_OUTPUT_FILE}.csv")
+        file_name = os.path.join(output_file_path, f"{file_name}.csv")
 
-        merged_summary_df.to_csv(file_name, index=False)
+        merged_df.to_csv(file_name, index=False)
 
 
 
 if __name__ == '__main__':
-    create_filelist()
-    files_list = remove_files()
-    appended_df = appending_files(files_list)
-    no_analysis_filelist = no_analysis_filelist()
-    appending_no_analysis_files(no_analysis_filelist, appended_df)
+    # Appending summary files
+    if Wave_PostProcessingOrchestra.RUN_APPEND_SUMMARY_FILES.lower() == 'yes':
+        summary_file_path = create_filelist(folder=config.INDIVIDUAL_SUM_F)
+        summary_files_list = remove_files(output_file=config.SUM_OUTPUT_FILE)
+        summary_appended_df = appending_files(summary_files_list, file_path=summary_file_path, append_level='summary')
+        no_analysis_files = no_analysis_filelist()
+        appending_no_analysis_files(no_analysis_files, summary_appended_df, file_name=config.SUM_OUTPUT_FILE)
 
+    # Appending hourly trimmed files
+    if Wave_PostProcessingOrchestra.RUN_APPEND_HOURLY_FILES.lower() == 'yes':
+        hourly_file_path = create_filelist(folder=config.INDIVIDUAL_TRIMMED_F)
+        hourly_files_list = remove_files(output_file=config.HOUR_OUTPUT_FILE)
+        hourly_appended_df = appending_files(hourly_files_list, file_path=hourly_file_path, append_level='hourly')
+        no_analysis_files = no_analysis_filelist()
+        appending_no_analysis_files(no_analysis_files, hourly_appended_df, file_name=config.HOUR_OUTPUT_FILE)
+
+    # Appending daily files
+    if Wave_PostProcessingOrchestra.RUN_APPEND_DAILY_FILES.lower() == 'yes':
+        daily_file_path = create_filelist(folder=config.INDIVIDUAL_DAILY_F)
+        daily_files_list = remove_files(output_file=config.DAY_OUTPUT_FILE)
+        daily_appended_df = appending_files(daily_files_list, file_path=daily_file_path, append_level='daily')
+        no_analysis_files = no_analysis_filelist()
+        appending_no_analysis_files(no_analysis_files, daily_appended_df, file_name=config.DAY_OUTPUT_FILE)
