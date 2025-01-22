6c8cf8cc4efe5ace62d47afa50a45d33701f66be
cas254 <Cecilie.Agergaard@mrc-epid.cam.ac.uk>
2025-01-22 15:27:39 +0000

    Update: Bat scripts updated to Acc Post Processing rather than Wave

---
+++ Acc_Post_Processing_Bat.bat
@@ -0,0 +1,2 @@
+"folder_path_example_where_python_is_installed\Python312\python.exe" "folder_path_example_where_acc_post_processing_script_is_saved\Python_Acc_Post_Processing\Acc_Post_ProcessingOrchestra.py"
+pause
